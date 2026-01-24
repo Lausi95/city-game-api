b8ef4f44fe4d0af9c56dabc505397d289fe7a7fd
Tom Lausmann <tomlausmann@gmail.com>
2026-01-24 14:28:05 +0100

    chore: remove unused variable

--- src/main/kotlin/net/lausi95/citygame/infrastructure/web/LoggingFilter.kt
+++ src/main/kotlin/net/lausi95/citygame/infrastructure/web/LoggingFilter.kt
@@ -1,6 +1,5 @@
 package net.lausi95.citygame.infrastructure.web
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -9,8 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.util.*
-
-private val log = KotlinLogging.logger { }
 
 @Component
 class LoggingFilter : OncePerRequestFilter() {
