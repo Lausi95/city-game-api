a5dce94c96b21d38a1f9b85f6d51eb49ed9edd03
Tom Lausmann <tomlausmann@gmail.com>
2026-01-24 18:13:01 +0100

    feat: add api documentation for create game endpoint

--- src/main/kotlin/net/lausi95/citygame/infrastructure/web/controller/game/GameController.kt
+++ src/main/kotlin/net/lausi95/citygame/infrastructure/web/controller/game/GameController.kt
@@ -1,9 +1,15 @@
 package net.lausi95.citygame.infrastructure.web.controller.game
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.headers.Header
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import net.lausi95.citygame.application.usecase.creategame.CreateGameRequest
 import net.lausi95.citygame.application.usecase.creategame.CreateGameUseCase
 import net.lausi95.citygame.domain.game.GameTitle
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,6 +24,23 @@ class GameController(
 ) {
 
     @PostMapping
+    @Operation(summary = "Creates a new game")
+    @ApiResponse(
+        responseCode = "201",
+        description = "Game was created successfully",
+        headers = [Header(name = "location", description = "URI of the new game")],
+        content = [],
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Input Validation Errors",
+        content = [Content(mediaType = "application/json", schema = Schema(ProblemDetail::class))],
+    )
+    @ApiResponse(
+        responseCode = "500",
+        description = "Internal Server Error",
+        content = [Content(mediaType = "application/json", schema = Schema(ProblemDetail::class))],
+    )
     fun postGame(
         @RequestBody @Valid requestDto: CreateGameRequestDto
     ): ResponseEntity<Unit> {
