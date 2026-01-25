254b1271c471a541293ade6f5877615a631da489
Tom Lausmann <tomlausmann@gmail.com>
2026-01-25 17:33:50 +0100

    chore: move use case test into correct package

--- src/test/kotlin/net/lausi95/citygame/application/usecase/game/creategame/CreateGameUseCaseTest.kt
+++ src/test/kotlin/net/lausi95/citygame/application/usecase/game/creategame/CreateGameUseCaseTest.kt
@@ -1,17 +1,15 @@
-package net.lausi95.citygame.application.usecase.creategame
+package net.lausi95.citygame.application.usecase.game.creategame
 
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.slot
 import io.mockk.verify
-import net.lausi95.citygame.application.usecase.game.creategame.CreateGameRequest
-import net.lausi95.citygame.application.usecase.game.creategame.CreateGameUseCase
 import net.lausi95.citygame.domain.game.Game
 import net.lausi95.citygame.domain.game.GameRepository
 import net.lausi95.citygame.domain.game.GameTitle
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -42,7 +40,7 @@ class CreateGameUseCaseTest {
         val game = slot<Game>()
         verify { gameRepository.save(capture(game)) }
 
-        assertSoftly {
+        SoftAssertions.assertSoftly {
             it.assertThat(game.captured.id).isEqualTo(response.gameId)
             it.assertThat(game.captured.title).isEqualTo(GameTitle("Foo"))
         }
@@ -60,6 +58,6 @@ class CreateGameUseCaseTest {
         }
 
         verify(exactly = 0) { gameRepository.save(any()) }
-        assertThat(exception.message).isEqualTo("Game with title already exist. Title: 'Foo'")
+        Assertions.assertThat(exception.message).isEqualTo("Game with title already exist. Title: 'Foo'")
     }
 }
